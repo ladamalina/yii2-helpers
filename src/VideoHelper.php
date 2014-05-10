61c1bb70a69fa86d76ed74a54d0a51c455aa89dc
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2014-05-10 17:04:55 +0400

    Update VideoHelper.php

--- src/VideoHelper.php
+++ src/VideoHelper.php
@@ -7,5 +7,33 @@ namespace yii\helpers;
  */
 class VideoHelper
 {
-	
+    protected static $hosts = ['youtube.com', 'www.youtube.com'];
+    protected static $id_params = ['v'];
+    protected static $hq_img = 'http://img.youtube.com/vi/{{id}}/hqdefault.jpg';
+
+	public static function getId($url) {
+        $url_parts = parse_url($url);
+
+        if (isset($url_parts['host']) and in_array($url_parts['host'], self::$hosts)) {
+            $query = $url_parts['query'];
+            $params = explode('&', $query);
+            foreach ($params as $pair) {
+                list($key, $value) = explode('=', $pair);
+                if (in_array($key, self::$id_params) && $value) {
+                    return $value;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static function getImg($url) {
+        $id = self::getId($url);
+        if ($id) {
+            return str_replace('{{id}}', $id, self::$hq_img);
+        }
+
+        return false;
+    }
 }
